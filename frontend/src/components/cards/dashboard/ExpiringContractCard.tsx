6df5f26f70ac0c0e32551a92580e356f7d3f185d
dionae1 <zguixmz@gmail.com>
2025-12-18 15:38:32 -0300

    fix: refactor dashboard service queries for improved performance and clarity, SQL > ORM

--- frontend/src/components/cards/dashboard/ExpiringContractCard.tsx
+++ frontend/src/components/cards/dashboard/ExpiringContractCard.tsx
@@ -1,7 +1,6 @@
 import { Contract } from "@/types/Contract";
 
 function ExpiringContractCard({ data }: { data: Contract }) {
-
     return (
         <div className="p-4 min-h-32 rounded-sm w-full md:w-auto flex-1 bg-white border border-slate-200 shadow-sm hover:shadow-md transition-all duration-200">
             <p className="font-semibold text-slate-800">{data.client.name}</p>
