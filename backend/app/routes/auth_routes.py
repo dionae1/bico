e2b71a4c77318109b40fbb5328bddec4e5235f6c
dionae1 <zguixmz@gmail.com>
2026-01-15 14:10:13 -0300

    fix: fixed wrong cookie path

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Request, status, Depends
+from fastapi import APIRouter, HTTPException, Request, Depends, Response, status
 from fastapi.responses import JSONResponse
 from app.services import user as user_service
 from app.schemas.user import ResponseUser, CreateUserRequest, LoginUserRequest
@@ -10,7 +10,7 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @router.post("/login", status_code=status.HTTP_200_OK)
-def login(user_in: LoginUserRequest, db: Session = Depends(get_db)):
+def login(response: Response, user_in: LoginUserRequest, db: Session = Depends(get_db)):
 
     user = user_service.authenticate_user(user_in.email, user_in.password, db=db)
     if not user:
@@ -21,30 +21,36 @@ def login(user_in: LoginUserRequest, db: Session = Depends(get_db)):
     access_token = auth.create_access_token(data={"sub": str(user.id)})
     refresh_token = auth.create_refresh_token(data={"sub": str(user.id)})
 
-    response = JSONResponse(
+    json_res = JSONResponse(
         {
             "access_token": access_token,
             "token_type": "bearer",
         }
     )
 
-    response.set_cookie(
+    json_res.set_cookie(
         key="refresh_token",
         value=refresh_token,
         httponly=True,
-        secure=True,
+        secure=False,
         samesite="Lax",
         max_age=24 * 60 * 60,
-        path="/api/v1/auth/refresh",
+        path="/",
     )
-    return response
+    
+    return json_res
 
 
 @router.post("/logout", status_code=status.HTTP_200_OK)
-def logout():
-    response = JSONResponse({"detail": "Successfully logged out"})
-    response.delete_cookie(key="refresh_token", path="/api/v1/auth/refresh")
-    return response
+def logout(response: Response):
+    response.delete_cookie(
+        key="refresh_token",
+        path="/",
+        httponly=True,
+        secure=False,
+        samesite="Lax",
+    )
+    return {"detail": "Successfully logged out"}
 
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
@@ -82,8 +88,9 @@ def demo_login(db: Session = Depends(get_db)):
 
 
 @router.post("/refresh", status_code=status.HTTP_200_OK)
-def refresh_token(request: Request, db: Session = Depends(get_db)):
+def refresh_token(request: Request, response: Response, db: Session = Depends(get_db)):
     refresh_token = request.cookies.get("refresh_token")
+    
     if not refresh_token or not auth.verify_refresh_token(refresh_token):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token"
@@ -92,19 +99,21 @@ def refresh_token(request: Request, db: Session = Depends(get_db)):
     access_token = auth.create_access_token(data={"sub": str(user.id)})
     refresh_token = auth.create_refresh_token(data={"sub": str(user.id)})
 
-    response = JSONResponse(
+    json_res = JSONResponse(
         {
             "access_token": access_token,
             "token_type": "bearer",
         }
     )
-    response.set_cookie(
+
+    json_res.set_cookie(
         key="refresh_token",
         value=refresh_token,
         httponly=True,
-        secure=True,
+        secure=False,
         samesite="Lax",
         max_age=24 * 60 * 60,
-        path="/api/v1/auth/refresh",
+        path="/",
     )
-    return response
+    
+    return json_res
